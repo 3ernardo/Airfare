5076eeb06466081ccee89813f0e5bea0f3eea64e
Bernardo <3ernardo@gmail.com>
2017-06-07 00:59:20 -0300

    Released the reserved word Connection from the package name.

---
+++ src/DAO/AirplaneDAO.java
@@ -0,0 +1,11 @@
+package DAO;
+
+import java.sql.Connection;
+import Connector.DBConnect;
+
+/**
+ * Created by Bernardo on 07/06/2017.
+ */
+public class AirplaneDAO {
+    private Connection connection;
+}
